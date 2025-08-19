3b89ca6d86c7c390d63f85e79ef3c712c6d64371
ant-post <ant.posternak@gmail.com>
2025-08-19 16:48:40 +0300

    feat: add UI and interactivity
    Created static layout using Flexbox. Implemented interactivity features
    and rewritten basic game logic using JavaScript.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-console.log("Hello, to play game type 'playGame()'");
-
 let humanScore = 0;
 let computerScore = 0;
+let roundsCounter = 0;
+const MAX_ROUNDS_NUMBER = 5;
+
+let lastHumanChoice;
+let lastComputerChoice;
+
+const GAME_JOURNAL = document.querySelector('.game-journal');
+const GAME_SCORE = document.querySelector('.game-score');
 
 const gameRules = {
-    "rock": "scissors",
-    "paper": "rock",
-    "scissors": "paper"
+    'rock': 'scissors',
+    'paper': 'rock',
+    'scissors': 'paper'
 };
  
-// get random int from 0 to max
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -17,83 +22,161 @@ function getRandomInt(max) {
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
         case 0:
-            return "rock";
+            return 'rock';
         case 1:
-            return "paper";
-        default: // case 2
-            return "scissors";
+            return 'paper';
+        default:
+            return 'scissors';
     }
 }
 
-function getHumanChoice() {
-    let humanChoice;
-    let attempts = 2;
-
-    do {
-        humanChoice = prompt("Input choice: rock / paper / scissors");
-        if (humanChoice === null) {
-            alert("It seems like you don't want to play :(");
-            break;
+function displayGamersStatus(humanScore, computerScore) {
+    if (roundsCounter >= MAX_ROUNDS_NUMBER) {
+        const HUMAN_GAME_STATUS = document.querySelector('.human-game-status');
+        const COMPUTER_GAME_STATUS = document.querySelector('.computer-game-status');
+       
+        if (humanScore === computerScore) {
+            HUMAN_GAME_STATUS.classList.add('game-status-draw');
+            COMPUTER_GAME_STATUS.classList.add('game-status-draw');
+                    
+            HUMAN_GAME_STATUS.textContent = 'Draw';
+            COMPUTER_GAME_STATUS.textContent = 'Draw';
         }
-        
-        humanChoice = humanChoice.toLowerCase();
-        if (humanChoice in gameRules) {
-            return humanChoice;
-        }
+        else if (humanScore > computerScore) {
+            HUMAN_GAME_STATUS.classList.add('game-status-victory');
+            COMPUTER_GAME_STATUS.classList.add('game-status-defeat');
 
+            HUMAN_GAME_STATUS.textContent = 'Win';
+            COMPUTER_GAME_STATUS.textContent = 'Loss';
+        }
         else {
-            if (attempts > 0) {
-                alert(`Wrong choice, try again. Attempts left: ${attempts}`);
-                attempts--;                 
-            }
-            else {
-                alert("It seems like you don't want to play :(");
-                break;
-            }
+            HUMAN_GAME_STATUS.classList.add('game-status-defeat');
+            COMPUTER_GAME_STATUS.classList.add('game-status-victory');
+
+            HUMAN_GAME_STATUS.textContent = 'Loss';
+            COMPUTER_GAME_STATUS.textContent = 'Win';
         }
-    } while (true);
-} 
+    }
+}
+
+function clearGamersStatus() {
+    const HUMAN_GAME_STATUS = document.querySelector('.human-game-status');
+    const COMPUTER_GAME_STATUS = document.querySelector('.computer-game-status');
+    
+    HUMAN_GAME_STATUS.textContent = '';
+    HUMAN_GAME_STATUS.classList.remove('game-status-draw', 'game-status-victory', 'game-status-defeat');
+    COMPUTER_GAME_STATUS.textContent = '';
+    COMPUTER_GAME_STATUS.classList.remove('game-status-draw', 'game-status-victory', 'game-status-defeat');
+}
+
+function startNewGame() {
+    humanScore = 0;
+    computerScore = 0;
+    roundsCounter = 0;
+
+    while (GAME_JOURNAL.firstChild) {
+        GAME_JOURNAL.removeChild(GAME_JOURNAL.firstChild);
+    }
+
+    GAME_SCORE.textContent = "0 : 0"
+}
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === undefined) {
-        return;
+    let return_code;
+    lastHumanChoice = humanChoice;
+    lastComputerChoice = computerChoice;
+
+    if (roundsCounter >= MAX_ROUNDS_NUMBER) {
+        startNewGame();
+        clearGamersStatus(); 
+        return -1;
     }
 
     if (humanChoice === computerChoice) {
-        return "Hmm, it's a draw";
-    } 
+        humanScore++;
+        computerScore++;
+        roundsCounter++;
+        return 0;
+    }
     else if (gameRules[humanChoice] == computerChoice) {
         humanScore++;
-        return "You won the round :)";
+        roundsCounter++;
+        return 1;
     }
     else {
         computerScore++;
-        return "You lost the round :("
+        roundsCounter++;
+        return 2;
     }
-} 
+}
 
-function playGame() {
-    let roundResult;
-    let i = 1;
-
-    do {
-        roundResult = playRound(getHumanChoice(), getComputerChoice());
-        if (roundResult) {
-            alert(roundResult);
-        }
-        i++;
-    } while (!(roundResult === undefined) && i <= 5)
-    
-    if (roundResult === undefined) {
-        console.info("You canceled the game :(");
+function getChoiceImageElement(CHOICE) {
+    const NEW_IMG_EL = document.createElement('img');
+    if (CHOICE === 'rock') {
+        NEW_IMG_EL.setAttribute('src', './images/stone-logo.svg');
     }
-    else if (humanScore === computerScore) {
-        console.info("It's a draw");
-    }
-    else if (humanScore > computerScore) {
-        console.info(`You won the game with score ${humanScore}:${computerScore}`);
+    else if (CHOICE === 'paper') {
+        NEW_IMG_EL.setAttribute('src', './images/paper-logo.svg');
     }
     else {
-        console.info(`You lost the game ${humanScore}:${computerScore}`);
-    }        
+        NEW_IMG_EL.setAttribute('src', './images/scissors-logo.svg');
+    }
+    
+    return NEW_IMG_EL;
 }
+
+function displayResults(ROUND_RESULT) {
+    GAME_SCORE.textContent = `${humanScore} : ${computerScore}`;
+    
+    const GAME_JOURNAL_RECORD = document.createElement('div');
+    GAME_JOURNAL_RECORD.classList.add('game-journal-record');
+    GAME_JOURNAL.appendChild(GAME_JOURNAL_RECORD);
+    
+    const HUMAN_RESULT = document.createElement('div');
+    const COMPUTER_RESULT = document.createElement('div');
+    GAME_JOURNAL_RECORD.appendChild(HUMAN_RESULT);
+    GAME_JOURNAL_RECORD.appendChild(COMPUTER_RESULT);
+    
+    const HUMAN_LOGO = document.createElement('img');
+    const COMPUTER_LOGO = document.createElement('img');
+    
+    HUMAN_LOGO.setAttribute('src', './images/human-logo.svg');
+    COMPUTER_LOGO.setAttribute('src', './images/robot-logo.svg');
+    
+    HUMAN_RESULT.appendChild(HUMAN_LOGO);
+    COMPUTER_RESULT.appendChild(COMPUTER_LOGO);
+    
+    if (ROUND_RESULT === 1) {
+        HUMAN_RESULT.classList.add('game-journal-victory');
+        COMPUTER_RESULT.classList.add('game-journal-defeat');
+    }
+    else if (ROUND_RESULT === 2) {
+        HUMAN_RESULT.classList.add('game-journal-defeat');
+        COMPUTER_RESULT.classList.add('game-journal-victory');
+    }
+    else {
+        HUMAN_RESULT.classList.add('game-journal-draw');
+        COMPUTER_RESULT.classList.add('game-journal-draw');
+    } 
+
+    HUMAN_RESULT.appendChild(getChoiceImageElement(lastHumanChoice));
+    COMPUTER_RESULT.appendChild(getChoiceImageElement(lastComputerChoice));
+}
+
+const CHOICE_BUTTONS = document.querySelectorAll('#rockButton, #paperButton, #scissorsButton');
+CHOICE_BUTTONS.forEach(button => {
+    button.addEventListener('click', () => {
+        const PLAYER_CHOICE = button.id.replace('Button', '');
+
+        const ROUND_RESULT = playRound(PLAYER_CHOICE, getComputerChoice());
+        if (ROUND_RESULT >= 0) {
+            displayResults(ROUND_RESULT);
+            displayGamersStatus(humanScore, computerScore);
+        }
+        
+        button.classList.add('choice-button-clicked');
+        setTimeout(() => {
+            button.classList.remove('choice-button-clicked');
+        }, 100)
+    })
+});
